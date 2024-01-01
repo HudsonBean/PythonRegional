8433953c206d841aa816797a3a286f64e8bf1b5e
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-01 15:31:06 -0600

    Reformatted into a class and object

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -17,6 +17,42 @@ class Pizzas:
       #Enter your code below
 #Variables
 pizzas = {}
+class Pizza:
+   def __init__(self, number):
+      def _getMenuCode():
+         while (True):
+            menuCode = input("Please enter a 3 letter/number menu code for pizza number " + str(number+1) +".\n>")
+            #SC2 Validity checks
+            #Check if menu code is in range
+            if (len(menuCode) == 3):
+               #Check for symbols
+               #Validity variable
+               canContinue = True
+               for letter in menuCode:
+                  if (letter.isdigit() or letter.isalpha()):
+                     pass
+                  else:
+                     #print("BOO!!!", letter)
+                     canContinue = False
+               if (canContinue == True):
+                  #Save data
+                  self.menuCode = menuCode
+                  break
+               else:
+                  print("\nCombination must only contain letters or numbers!\n\n")
+                  continue
+            else:
+               print("\nCombination must have 3 letters or numbers!\n\n")
+               continue
+      def _getMenuName():
+         #SC3 get the name of the menu item
+         menuName = input("Please enter the menu name for " + self.menuCode + ".\n>")
+         
+      def _getInventoryQuantity():
+         print(str(number))
+      _getMenuCode()
+      _getMenuName()
+      _getInventoryQuantity()
 #SC1 Prompt the user to input the number of simulated pizzas they would like
 #Set up exception check loop
 while (True):
@@ -37,40 +73,12 @@ while (True):
       print("\nPlease enter a number!\n\n")
       continue
 
-#SC2 Get some menu codes for the pizzas they want generated
+#SC2 Get menu codes, names, and quanities of the pizzas
 #Create an interation variable
 i = 0
 while (i < numberOfPizzas):
    #pizzas[i]
    #Menu code for item i
-   while (True):
-      menuCode = input("Please enter a 3 letter/number menu code for pizza number " + str(i+1) +".\n>")
-      #SC2 Validity checks
-      #Check if menu code is in range
-      if (len(menuCode) == 3):
-         #Check for symbols
-         #Validity variable
-         canContinue = True
-         for letter in menuCode:
-            if (letter.isdigit() or letter.isalpha()):
-               pass
-            else:
-               #print("BOO!!!", letter)
-               canContinue = False
-         if (canContinue == True):
-            #Save data
-            #Create dict for my data
-            pizzas[i] = {}
-            #Add the menuCode to the data
-            pizzas[i]["menuCode"] = menuCode
-            break
-         else:
-            print("\nCombination must only contain letters or numbers!\n\n")
-            continue
-      else:
-         print("\nCombination must have 3 letters or numbers!\n\n")
-         continue
+   pizzas[i] = Pizza(i)
    #Increment the variable to target next item on the next iteration
    i+=1
-   
-print(pizzas)
