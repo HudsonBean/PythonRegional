72d5d670a0b186eec5fca6282883d46bc8286b21
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-01 15:16:10 -0600

    Finished menu code section

--- PizzaOrder.py
+++ PizzaOrder.py
@@ -16,11 +16,11 @@ class Pizzas:
 #DRIVER SECTION
       #Enter your code below
 #Variables
-      
+pizzas = {}
 #SC1 Prompt the user to input the number of simulated pizzas they would like
-numberOfPizzas = input("Hello! How many simulated pizzas would you like to create? (Must be between 1 and 10)\n>")
 #Set up exception check loop
 while (True):
+   numberOfPizzas = input("Hello! How many simulated pizzas would you like to create? (Must be between 1 and 10)\n>")
    #SC1 Check the validity of the user input
    #Check if is int
    if (numberOfPizzas.isnumeric()):
@@ -28,7 +28,7 @@ while (True):
       numberOfPizzas = int(numberOfPizzas)
       #Check if is in range
       if (numberOfPizzas >= 1 and numberOfPizzas <= 10):
-         print("\nYou entered: " + str(numberOfPizzas))
+         print("\nYou entered: " + str(numberOfPizzas) + "\n")
          break
       else:
          print("\nPlease enter a number between 1 and 10!\n\n")
@@ -38,6 +38,39 @@ while (True):
       continue
 
 #SC2 Get some menu codes for the pizzas they want generated
-for i in 3:
-   print(i)
+#Create an interation variable
+i = 0
+while (i < numberOfPizzas):
+   #pizzas[i]
+   #Menu code for item i
+   while (True):
+      menuCode = input("Please enter a 3 letter/number menu code for pizza number " + str(i+1) +".\n>")
+      #SC2 Validity checks
+      #Check if menu code is in range
+      if (len(menuCode) == 3):
+         #Check for symbols
+         #Validity variable
+         canContinue = True
+         for letter in menuCode:
+            if (letter.isdigit() or letter.isalpha()):
+               pass
+            else:
+               #print("BOO!!!", letter)
+               canContinue = False
+         if (canContinue == True):
+            #Save data
+            #Create dict for my data
+            pizzas[i] = {}
+            #Add the menuCode to the data
+            pizzas[i]["menuCode"] = menuCode
+            break
+         else:
+            print("\nCombination must only contain letters or numbers!\n\n")
+            continue
+      else:
+         print("\nCombination must have 3 letters or numbers!\n\n")
+         continue
+   #Increment the variable to target next item on the next iteration
+   i+=1
    
+print(pizzas)
